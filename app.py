c8ec3467c27ffe8f3b981706269337a2cc32e86a
Anh Le <leanh1188@gmail.com>
2020-08-29 10:30:58 +0200

    Add navbar, requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template('index.html', nachricht='Hello World!')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
